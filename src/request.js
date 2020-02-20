df103f792c31da23c4f4955902dd5bedc034e997
李富荣 <leefurong@gmail.com>
2020-02-20 21:13:47 +0800

    删除前端requests.js中对doctors.js的引用。

--- src/request.js
+++ src/request.js
@@ -1,9 +1,8 @@
 import sha256 from 'js-sha256';
 import axios from 'axios';
 import { groups } from './groups';
-import { doctors } from './doctors';
 import { contains } from './utils';
-import Qs from 'qs'
+import Qs from 'qs';
 axios.defaults.withCredentials = true;
 axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
 const api = {
@@ -51,15 +50,19 @@ export async function fetchGroup(groupName) {
   // TODO 连接服务器的事情搞定后， 我们把服务器上的数据和本地的比对。
   // 将医生的状态设为在线/离线/忙线。
   // dev
-  const { data } = await axios('/doctors')
+  const { data } = await axios('/doctors');
   // prod
   // const { data } = await axios('http://127.0.0.1:3001/doctors')
-  console.log(data)
+  console.log(data);
   if (data.returnCode === 0) {
-    return data.arr.filter(
-      groupName === '*' ? _ => true : doctor => contains(doctor.tags, groupName)
-    ).map(injectAvatar)
+    return data.arr
+      .filter(
+        groupName === '*'
+          ? _ => true
+          : doctor => contains(doctor.tags, groupName)
+      )
+      .map(injectAvatar);
   } else {
-    return []
+    return [];
   }
 }
