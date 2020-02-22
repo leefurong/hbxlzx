a86f2043fa1aeb160bfe626d83f883bc3f3e884b
李富荣 <leefurong@gmail.com>
2020-02-23 00:58:01 +0800

    添加第二批专家

--- src/server/doctors.js
+++ src/server/doctors.js
@@ -1,128 +1,151 @@
-var doctors = [{
+var doctors = [
+  {
     name: '塔塔',
     intro: 'ICF国际教练联合会专业认证教练',
     tags: 'ABCDEF',
     staffId: 239841,
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239841'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239841'
   },
   {
     name: '俞禾',
     intro: '国内首位精通情绪认证教练 \n精通情绪认证培训师 ',
     tags: 'ADF',
     staffId: 239851,
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239851'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239851'
   },
   {
     name: '王鑫',
     intro: 'WorkFace社会大学联合创始人 \n小浦那国际静修社区CEO ',
     tags: 'ADEF',
     staffId: 239861,
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239861'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239861'
   },
   {
     name: '高丽',
     intro: '心领导力教练\n正念带领导师\n具有丰富的政府及企业管理工作经验',
     tags: 'DEF',
     staffId: 239871,
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239871'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239871'
   },
   {
     name: '赵逸轩',
     intro: '埃里克森国际教练协会认证教练',
     tags: 'ADEF',
     staffId: 239881,
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239881'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239881'
   },
   {
     name: '张皓',
     intro: '国家二级心理咨询师\n杭州市家庭教育指导热线咨询教师',
     tags: 'ACDF',
     staffId: 239891,
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239891'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239891'
   },
   {
     name: '高正坤',
-    intro: '美国心脏数理研究所认证国际情绪教练\nICF认证Asia Coach Center教练\n美国NLP协会认证专业NLP教练\n美国TLTA时间线疗法专业执行师\n资深心理咨询师',
+    intro:
+      '美国心脏数理研究所认证国际情绪教练\nICF认证Asia Coach Center教练\n美国NLP协会认证专业NLP教练\n美国TLTA时间线疗法专业执行师\n资深心理咨询师',
     tags: 'EF',
     staffId: 239901,
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239901'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239901'
   },
   {
     name: '张骞',
     intro: '心理咨询师\n生涯规划师',
     tags: 'E',
     staffId: 239761,
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239761'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239761'
   },
   {
     name: '李雨健',
-    intro: 'ICF国际教练联盟认证进化教练\n4C团队认证教练\n亚太青少年营地认证教练\n中华传统文化教师',
+    intro:
+      'ICF国际教练联盟认证进化教练\n4C团队认证教练\n亚太青少年营地认证教练\n中华传统文化教师',
     tags: 'CDEF',
     staffId: 239911,
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239911'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239911'
   },
   {
     name: '向陈',
     intro: '国家二级人力资源师\n2019年完成ICF进化教练课程学习并取得结业证书',
     tags: 'CF',
     staffId: 239921,
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239921'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239921'
   },
   {
     name: '刘飞',
     intro: '呆不住正念主题酒店 创始人\nWorkFace设计师社群召集人',
     tags: 'EF',
     staffId: 239931,
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239931'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239931'
   },
   {
     name: '张紫霞',
-    intro: '埃里克森国际教练学院专业教练/教练型培训师\nICF国际教练联盟认证准PCC教练\n高管教练及企业内训师',
+    intro:
+      '埃里克森国际教练学院专业教练/教练型培训师\nICF国际教练联盟认证准PCC教练\n高管教练及企业内训师',
     tags: 'ADEF',
     staffId: 239941,
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239941'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239941'
   },
   {
     name: '赵霞',
-    intro: '加拿大埃里克森学院认证专业教练\n美国ICF准PCC教练\n芬兰儿童技能教养法（KS)专业讲师\nOH卡企业带领师\nOH卡教练\n高级人力资源管理师\nLOG视觉引导师',
+    intro:
+      '加拿大埃里克森学院认证专业教练\n美国ICF准PCC教练\n芬兰儿童技能教养法（KS)专业讲师\nOH卡企业带领师\nOH卡教练\n高级人力资源管理师\nLOG视觉引导师',
     tags: 'C',
     staffId: 239771,
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239771'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239771'
   },
   {
     name: '陈亭如',
     intro: '美国佩丁大学MBA\n国家二级心理咨询师\n美国AACTP国际注册培训师',
     tags: 'DF',
     staffId: 239951,
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239951'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239951'
   },
   {
     name: '董水霞',
     intro: '国家三级社会工作者\n国家三级心理咨询师',
     tags: 'DF',
     staffId: 239961,
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239961'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239961'
   },
   {
     name: '金怡',
     intro: 'NLP创作疗愈教练\n整体能量平衡（OPH证书）',
     tags: 'DF',
     staffId: 239971,
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239971'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239971'
   },
   {
     name: '方宗贤',
     intro: '国家二级心理咨询师\n进化教练',
     tags: 'DF',
     staffId: 239981,
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239981'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239981'
   },
   {
     name: '许丽丽',
     intro: 'ICF国际认证教练\n注册国际心理咨询师\n美国注册财务规划师',
     tags: 'DF',
     staffId: 239991,
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239991'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239991'
   },
   {
     name: '王丽',
@@ -131,140 +154,166 @@ var doctors = [{
 人际沟通分析学（TA）教练`,
     tags: 'DF',
     staffId: 240001,
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240001'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240001'
   },
   {
     name: '马云霞',
-    intro: '国家二级心理咨询师\n国家企业教练\nPMP项目管理师\n国家ISO质量管理体系审核师',
+    intro:
+      '国家二级心理咨询师\n国家企业教练\nPMP项目管理师\n国家ISO质量管理体系审核师',
     tags: 'DF',
     staffId: 240011,
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240011'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240011'
   },
   {
     name: '陈修文',
-    intro: '国家注册二级心理咨询师\n临床催眠治疗师\n催眠协会会员\n沙盘游戏治疗师\n中国心理分析研究会会员',
+    intro:
+      '国家注册二级心理咨询师\n临床催眠治疗师\n催眠协会会员\n沙盘游戏治疗师\n中国心理分析研究会会员',
     tags: 'DF',
     staffId: 240021,
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240021'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240021'
   },
   {
     name: '范赢',
-    intro: '未来呈现师\n高管教练PCC/团队教练\n企业变革/高绩效团队顾问\n幸福企业家倡导者',
+    intro:
+      '未来呈现师\n高管教练PCC/团队教练\n企业变革/高绩效团队顾问\n幸福企业家倡导者',
     tags: 'AE',
     staffId: 240031,
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240031'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240031'
   },
   {
     name: '宋依兰',
-    intro: 'ICF国际教练联合会认证PCC教练\n美国情绪压力管理研究中心自我情绪管理系统训练教练\n创问中国教练中心4C团队教练课程导师\n美国麻省理工自然流现学院SPT合格教练',
+    intro:
+      'ICF国际教练联合会认证PCC教练\n美国情绪压力管理研究中心自我情绪管理系统训练教练\n创问中国教练中心4C团队教练课程导师\n美国麻省理工自然流现学院SPT合格教练',
     tags: 'ACDEF',
     staffId: 240041,
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240041'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240041'
   },
   {
     name: '徐珺泽',
     intro: 'ICF国际教练联合会认证教练\n生命自传教练（3年项目受训中）\nOH卡教练',
     tags: 'ABDEF',
     staffId: 240051,
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240051'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240051'
   },
   {
     name: '孙馨',
-    intro: '南京理工大学英语系专业毕业\n2019开始接触学习亲密关系课程，善于做课程总结',
+    intro:
+      '南京理工大学英语系专业毕业\n2019开始接触学习亲密关系课程，善于做课程总结',
     tags: 'C',
     staffId: 239781,
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239781'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239781'
   },
   {
     name: '卢春霞',
     intro: '埃里克森国际认证教练\nICF国际教练联合会专业认证教练',
     tags: 'F',
     staffId: 239791,
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239791'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239791'
   },
   {
     name: '菁珂',
     intro: '埃里克森国际认证教练\n国家注册中级生涯规划师',
     tags: 'CE',
     staffId: 240061,
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240061'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240061'
   },
   {
     name: '张月欣',
     intro: '国家二级心理咨询师',
     tags: 'CE',
     staffId: 240071,
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240071'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240071'
   },
   {
     name: '孙千越',
     intro: '国家二级心理咨询师',
     tags: 'E',
     staffId: 239801,
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239801'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239801'
   },
   {
     name: '彭晶生',
     intro: '情绪心态专业教练\n擅长情绪心态调整，有800+个案经验',
     tags: 'ADEF',
     staffId: 240081,
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240081'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240081'
   },
   {
     name: '刘霞',
     intro: '埃里克森国际教练学院专业教练\nNLP学院高级企业教练国际认证',
     tags: 'F',
     staffId: 239811,
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239811'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239811'
   },
   {
     name: '俞浩',
-    intro: '英国爱丁堡大学培训与发展专业硕士\nICF认证PCC专业教练\nDDI领导力课程认证培训师\nNLP认证执行师\nMBTI认证讲师',
+    intro:
+      '英国爱丁堡大学培训与发展专业硕士\nICF认证PCC专业教练\nDDI领导力课程认证培训师\nNLP认证执行师\nMBTI认证讲师',
     tags: 'D',
     staffId: 239821,
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239821'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239821'
   },
   {
     name: '杨兰',
     intro: '埃里克森国际学院认证专业教练（EPC）\nICF国际教练联盟认证教练',
     tags: 'EF',
     staffId: 240101,
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240101'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240101'
   },
   {
     name: '吴菁飞',
     intro: 'ICF国际教练机构认证专业生命教练',
     tags: 'B',
     staffId: 239831,
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239831'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=239831'
   },
   {
     name: '郑铮一',
     intro: '国家二级心理咨询师',
     tags: 'DF',
     staffId: 240111,
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240111'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240111'
   },
   {
     name: '辛欣',
     intro: 'ICF国际教练联盟认证教练\nDISC、九型人格、盖洛普优势认证培训师',
     tags: 'DF',
     staffId: 240121,
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240121'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240121'
   },
   {
     name: '毛霞文',
     intro: '主管护师\n本科学历\n健康管理师三级\n现任邵逸夫医院护理教育部',
     tags: 'DF',
     staffId: 240131,
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240131'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240131'
   },
   {
     name: '刘海舰',
     intro: '企业教练高级',
     tags: 'DF',
     staffId: 240141,
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240141'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240141'
   },
   {
     name: '徐智泓',
@@ -275,7 +324,8 @@ var doctors = [{
 美国六秒钟情商实践家
 案例超过700小时`,
     tags: 'ABDEF',
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240831'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240831'
   },
   {
     name: '吴思',
@@ -283,7 +333,8 @@ var doctors = [{
     intro: `08年 5.12支援汶川抗震救灾
 国家二级心理咨询师`,
     tags: 'E',
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240841'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240841'
   },
 
   {
@@ -293,7 +344,8 @@ var doctors = [{
 中科院心理研究所 心理教练
 案例超过300个小时`,
     tags: 'ACEF',
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240821'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240821'
   },
   {
     name: '黄小仙',
@@ -302,7 +354,8 @@ var doctors = [{
 主管护师职称
 注重身心健康调理`,
     tags: 'B',
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240811'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240811'
   },
 
   {
@@ -312,7 +365,8 @@ var doctors = [{
 组织发展顾问
 项目运营总监`,
     tags: 'ACDEF',
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240861'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240861'
   },
   {
     name: '徐金亮',
@@ -320,7 +374,8 @@ var doctors = [{
     intro: `教育工作者
 情绪管理顾问导师`,
     tags: 'E',
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240931'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240931'
   },
   {
     name: '戎静',
@@ -330,7 +385,149 @@ var doctors = [{
 4D领导力
 15年案例超过2千小时`,
     tags: 'ACDE',
-    supportURL: 'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240801'
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=240801'
+  },
+
+  {
+    name: '巩晓宁',
+    staffId: 241141,
+    intro: '二级心理咨询师',
+    avatar: 1,
+    tags: 'ABC',
+    email: '13802969469@163.com',
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=241141'
+  },
+  {
+    name: '贺华文',
+    staffId: 241151,
+    intro: '高管教练、成长教练、情绪教练',
+    avatar: 1,
+    tags: 'ABE',
+    email: '34836700@qq.com',
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=241151'
+  },
+  {
+    name: '欧阳彦琨',
+    staffId: 241161,
+    intro: '东方心教练、埃里克森教练、生涯教练',
+    avatar: 1,
+    tags: 'AF',
+    email: '183272922@qq.com',
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=241161'
+  },
+  {
+    name: '邢楠',
+    staffId: 241171,
+    intro: '东方心教练',
+    avatar: 1,
+    tags: 'ABCDE',
+    email: '525180274@qq.com',
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=241171'
+  },
+  {
+    name: '魏奕',
+    staffId: 241181,
+    intro: '东方心教练',
+    avatar: 1,
+    tags: 'ABCDEF',
+    email: 'weiyi2000@hotmail.com',
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=241181'
+  },
+  {
+    name: '周桂娟',
+    staffId: 241191,
+    intro: '东方心教练',
+    avatar: 1,
+    tags: 'ABCDEF',
+    email: '444895377@qq.com',
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=241191'
+  },
+  {
+    name: '何芸',
+    staffId: 241201,
+    intro: '东方心教练',
+    avatar: 1,
+    tags: 'ABDE',
+    email: 'heyun_112@163.com',
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=241201'
+  },
+  {
+    name: '慈冰冰',
+    staffId: 241211,
+    intro: 'NLP教练、国家二级心理咨询师',
+    avatar: 1,
+    tags: 'ABEF',
+    email: 'ci.bingbing@163.com',
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=241211'
+  },
+  {
+    name: '陈莹',
+    staffId: 241221,
+    intro: '东方心教练、共创教练',
+    avatar: 1,
+    tags: 'DEF',
+    email: 'audreychenying@163.com',
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=241221'
+  },
+  {
+    name: '张瑛',
+    staffId: 241231,
+    intro: '二级心理咨询师、教练',
+    avatar: 1,
+    tags: 'ADE',
+    email: 'shunshunmamibao@hotmail.com',
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=241231'
+  },
+  {
+    name: '李柯睿',
+    staffId: 241241,
+    intro: '东方心教练，埃里克森教练',
+    avatar: 1,
+    tags: 'ABDEF',
+    email: '359688234@qq.com',
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=241241'
+  },
+  {
+    name: '赵海亮',
+    staffId: 241251,
+    intro: '企业教练．静心者',
+    avatar: 1,
+    tags: 'DE',
+    email: '499142459@qq.com',
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=241251'
+  },
+  {
+    name: '杨莉珺',
+    staffId: 241261,
+    intro: 'ICF认证PCC教练',
+    avatar: 1,
+    tags: 'AE',
+    email: '18621952383@163.com',
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=241261'
+  },
+  {
+    name: '吴翠',
+    staffId: 241271,
+    intro: '东方心教练，埃里克森教练，CCF高级教练',
+    avatar: 1,
+    tags: 'CE',
+    email: '185935812@qq.com',
+    supportURL:
+      'https://hbxlzx.udesk.cn/im_client/?web_plugin_id=115241&agent_id=241271'
   }
 ];
 
